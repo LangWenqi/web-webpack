4f1a79f5a574c656b8041540400e01cfcb304e74
LangWenqi <497379037@qq.com>
2020-03-09 16:31:55 +0800

    规避域名
    规避域名

--- src/utils/variable.js
+++ src/utils/variable.js
@@ -2,21 +2,13 @@ export const ACCESS_TOKEN = 'accessToken';
 export const REFRESH_TOKEN = 'refresh_token';
 export const ID_TOKEN = 'id_token';
 export const EXP = 'exp';
-// 打开的文件和工作台通信分类
-export const OFFICE_HANDLE = {
-	RENAME_KEY: 'rename', // 重命名
-	NEW_KEY: 'new', // 新建
-	READED_KEY: 'readed' // 标记已读
-};
+
 // 请求域名
 export function GET_BASE_URL () {
-	return process.env.NODE_ENV === 'production' ? window.location.origin : 'https://docs.bianfeng.com';
+	return process.env.NODE_ENV === 'production' ? window.location.origin : 'https://www.baidu.com';
 };
 // 网站域名
 export function GET_WEB_URL () {
-	return process.env.NODE_ENV === 'production' ? `${window.location.origin}/` : 'http://192.168.137.22:1024/';
+	return process.env.NODE_ENV === 'production' ? `${window.location.origin}/` : 'http://192.168.xxx.xx:xxxx/';
 };
-// onlyoffice js 文件域名
-export function GET_OFFICE_JS_URL () {
-	return process.env.NODE_ENV === 'production' ? `${window.location.origin}/ds-vpath/web-apps/apps/api/documents/api.js` : 'https://docs.bianfeng.com/ds-vpath/web-apps/apps/api/documents/api.js';
-};
+
